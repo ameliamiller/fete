0c90abf1b17e0b3e2d911eab6c0bfa0d3a3ad6f7
Amelia Miller <ameliamiller@Amelias-MacBook-Air-2.local>
2026-05-06 14:46:41 -0400

    change favicon to red balloon

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "fete 🎉",
   description: "Simple, beautiful event invites.",
-  icons: { icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎉</text></svg>" },
+  icons: { icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎈</text></svg>" },
 };
 
 export const viewport: Viewport = {
